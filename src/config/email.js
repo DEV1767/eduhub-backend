02ea23183a59183f75083f6d581ebcc6316c4fea
shivam <shivam7667@gmail.com>
2026-03-26 19:25:17 +0530

    otp

--- src/config/email.js
+++ src/config/email.js
@@ -1,17 +1,17 @@
 import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
-  host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
+    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
 
-  auth: {
-    user: process.env.APP_EMAIL,
-    pass: process.env.APP_EMAIL_PASSWORD,
-  },
+    auth: {
+        user: process.env.APP_EMAIL,
+        pass: process.env.APP_EMAIL_PASSWORD,
+    },
 
-  family: 4   // ✅ THIS LINE FIXES YOUR ISSUE
+    family: 4   // ✅ THIS LINE FIXES YOUR ISSUE
 });
 
 export default transporter
